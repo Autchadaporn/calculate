ffa2cb9c0d85dc66fa390422b347fad17a4024a3
ing <inging.510801@gmail.com>
2020-06-06 15:02:19 +0700

    update

--- connect.py
+++ connect.py
@@ -28,18 +28,54 @@ def calculate():
     rows=cur.fetchall()
     # for row in rows :
     #     print(row)
-
-    Subject = request.form.getlist("Subject[]") #รับค่าเป็นarray
+ 
+    Subject = request.form.getlist("Subject[]") #รับค่าเป็น list จากform index.html
     Unit = request.form.getlist("Unit[]")
     Grade = request.form.getlist("Grade[]")
+
+    # ---------------------- start  ส่งค่าแล้วprintออกมาเป็นjson -------------------------
+    # headers = ('Subject', 'Unit', 'Grade')
+    # values = (
+    #     request.form.getlist('Subject[]'),  
+    #     request.form.getlist('Unit[]'),  
+    #     request.form.getlist('Grade[]'),         
+    # )
+    # items = [{} for i in range(len(values[0]))]
+    # for x,i in enumerate(values):  #enumerate เป็นคำสั่งสำหรับแจกแจงค่า index และข้อมูลใน index ในรูปแบบทูเพิล (Tuple) ดังนี้ (Index,Value) โดยต้องใช้กับข้อมูลชนิด list
+    #     for _x,_i in enumerate(i): 
+    #         items[_x][headers[x]] = _i
+    # return jsonify(items)
+    # -------------------- stop  ส่งค่าแล้วprintออกมาเป็นjson-----------------------
+
+    print("***********************************************************************")
+    print('วิชา:',Subject)
+    print('หน่วยกิต:',Unit)
+    print('เกรด:',Grade)
+    print("***********************************************************************")
+   
+    #------------------------- start หาค่าผลรวมของหน่วยกิต ---------------------------
+    sum =0
+    for x in range(len(Unit)):
+        #print(float(Unit[x])) #หน่วยกิตแต่ละตัว อ้างจาก x คือ index
+        sum = sum + (float(Unit[x]))
+    print('ผลรวมหน่วยกิต =',sum)
+    #--------------------------- Stop หาค่าผลรวมของหน่วยกิต -------------------------
     
     print("***********************************************************************")
-    print(Subject,Unit,Grade)
-    # Total1 = (float(Unit) * float(Grade))
-    # print (Subject,Total1)
+    total =0 
+    for x in range(len(Unit)):
+        for i in range(len(Grade)):
+            if i == x: #ถ้า index ของ Unit และ Grade เท่ากัน ให้นำมาคำนวณ
+                sum1 = (float(Unit[x])* float(Grade[i])) # หน่วยกิต คูณ เกรด
+                # print(sum1)
+                total = total+sum1 # หน่วยกิต คูณ เกรด มา บวกกัน เช่น (3*4.0)+(3*3.5)
+    print('ผลรวม หน่วยกิต*เกรด =',total)
     print("***********************************************************************")
+    GPA = total / sum
+    GPA = '%.2f'%(GPA) # ตัดทศนิยมให้เหลือ 2 ตำแหน่ง เช่น 2.9642857142857144 เป็น 2.96
+    print(GPA)
     
-    return render_template("index.html",datas=rows)
+    return render_template("index.html",datas=rows,item=GPA)
 
 if __name__== "__main__" :
     app.run(debug=True)
